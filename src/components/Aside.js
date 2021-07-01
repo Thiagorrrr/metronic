b0ee021d3d0788265bb3f4a90dfc457a118dcad7
thiagorrrr <thiago.pereira.053@gmail.com>
2021-07-01 01:50:28 -0300

    ajuste de scroll

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -3,45 +3,47 @@ import Accordion from "./Accordion";
 import dados from "../dados/data";
 import Arrow from "../svg/Arrow"
 
-const Aside = ({activeMenu}) => {
+const Aside = ({ activeMenu }) => {
   const [close, setClose] = useState(false);
   const [mouse, setMouse] = useState(false);
 
   return (
-    <aside className={`aside ${activeMenu ? 'aside--active': ''}  ${close ? `aside--close` : ''}`}>
-      <div className="aside__container">
-        <div className="aside__brand d-none d-lg-flex">
-          <span className="aside__arrow" onClick={() => setClose(close ? false : true)}>
-            <Arrow />
-          </span>
-        </div>
-        <ul className="aside__list" id="asideMenu"
-          onMouseEnter={() => {
-            return (
-              close === true ? setClose(false) : null,
-              close === true ? setMouse(true) : setMouse(false)
-            )
-          }}
-          onMouseLeave={() => mouse === true ? setClose(true) : null}
-        >
-          {
-            dados.aside.map((item, index) => {
+    <aside className={`aside ${activeMenu ? 'aside--active' : ''}  ${close ? `aside--close` : ''}`}>
+      <div className={`aside__fixed ${activeMenu ? 'aside__fixed--active' : ''}  ${close ? `aside__fixed--close` : ''}`}>
+        <div className="aside__container">
+          <div className="aside__brand d-none d-lg-flex">
+            <span className="aside__arrow" onClick={() => setClose(close ? false : true)}>
+              <Arrow />
+            </span>
+          </div>
+          <ul className="aside__list" id="asideMenu"
+            onMouseEnter={() => {
               return (
-                <Accordion
-                  close={close}
-                  key={index}
-                  index={index}
-                  name={item.name}
-                  text={item.text}
-                  icon={item.icon}
-                  submenu={close ? '' : item.submenu}
-                  selected={item.selected}
-                  classe={item.classe}
-                />
+                close === true ? setClose(false) : null,
+                close === true ? setMouse(true) : setMouse(false)
               )
-            })
-          }
-        </ul>
+            }}
+            onMouseLeave={() => mouse === true ? setClose(true) : null}
+          >
+            {
+              dados.aside.map((item, index) => {
+                return (
+                  <Accordion
+                    close={close}
+                    key={index}
+                    index={index}
+                    name={item.name}
+                    text={item.text}
+                    icon={item.icon}
+                    submenu={close ? '' : item.submenu}
+                    selected={item.selected}
+                    classe={item.classe}
+                  />
+                )
+              })
+            }
+          </ul>
+        </div>
       </div>
     </aside>
   )
